574417f3b8896c6c16fa7739f443264de8843809
aneeshmaury <aneeshmaury.in@gmail.com>
2024-12-10 12:12:59 +0530

    kflddfl

--- app/nav.js
+++ app/nav.js
@@ -7,15 +7,15 @@ import { FaArrowLeft } from "react-icons/fa6";
 const HeaderBar = () => {
   return (
     <div className="bg-[#6A1B9A] text-white flex items-center justify-between p-4">
-      <div className="flex items-center gap-6">
-         <div><FaArrowLeft /> </div>
-      <div className="text-[18px]">Help</div>
+      <div className="flex items-center gap-8 pl-3">
+         <div className="text-[20px]"><FaArrowLeft /> </div>
+      <div className="text-[20px]">Help</div>
       </div>
      
 
       <div>
-<div className="flex items-center gap-6">
-     <p className="font-[14px]">VIEW TICKETS</p>
+<div>
+     <p className="font-[18px] ">VIEW TICKETS</p>
       </div>
 
       </div>
