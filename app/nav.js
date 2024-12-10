323351b6d42e2a0d3e694894aa3bf7c1eb35dc4c
aneeshmaury <aneeshmaury.in@gmail.com>
2024-12-10 14:46:24 +0530

    fgdfgdf

--- app/nav.js
+++ app/nav.js
@@ -2,11 +2,12 @@ import React from "react";
 import { FaFacebookF, FaBell } from "react-icons/fa"; // Facebook and notification icons
 import { MdMessage } from "react-icons/md"; // Message icon
 import { FaArrowLeft } from "react-icons/fa6";
-
+import { TiArrowSortedDown } from "react-icons/ti";
 
 const HeaderBar = () => {
   return (
-    <div className="bg-[#6A1B9A] text-white flex items-center justify-between p-4">
+    <div>
+       <div className="bg-[#6A1B9A] text-white flex items-center justify-between p-4">
       <div className="flex items-center gap-8 pl-3">
          <div className="text-[20px]"><FaArrowLeft /> </div>
       <div className="text-[20px]">Help</div>
@@ -19,8 +20,23 @@ const HeaderBar = () => {
       </div>
 
       </div>
-      
+</div>
+
+
+  <div className="flex items-center justify-between  pl-3 pr-1  py-[2px] mt-2 mr-4  rounded-lg border place-self-end align-center w-[90px] ">
+     <p>English</p>
+     <div className="text-[15px]"><TiArrowSortedDown /></div>
+  </div>
+  <div class="mt-2">
+  <hr class="border-gray-300 shadow-lg" />
+</div>
+
+
+
     </div>
+   
+
+
   );
 };
 
