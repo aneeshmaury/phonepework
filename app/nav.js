53546f89008abe9410553eaf9f833b4643f9f26f
aneeshmaury <aneeshmaury.in@gmail.com>
2024-12-10 11:58:18 +0530

    fgdfgkh

--- app/nav.js
+++ app/nav.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { FaFacebookF, FaBell } from "react-icons/fa"; // Facebook and notification icons
 import { MdMessage } from "react-icons/md"; // Message icon
+import { FaArrowLeft } from "react-icons/fa6";
+
 
 const HeaderBar = () => {
   return (
-    <div className="bg-purple-700 text-white flex justify-between items-center p-4">
-      {/* Left Section */}
-      <div className="text-lg font-bold">Help</div>
-
-      {/* Right Section */}
+    <div className="bg-[#6A1B9A] text-white flex items-center justify-between p-4">
       <div className="flex items-center gap-6">
-        <FaFacebookF className="text-xl cursor-pointer hover:scale-110 transition-transform duration-200" />
-        <FaBell className="text-xl cursor-pointer hover:scale-110 transition-transform duration-200" />
-        <MdMessage className="text-xl cursor-pointer hover:scale-110 transition-transform duration-200" />
+         <div><FaArrowLeft /> </div>
+      <div className="text-lg">Help</div>
       </div>
+     
+
+      <div>
+<div className="flex items-center gap-6">
+     <p>VIEW TICKETS</p>
+      </div>
+
+      </div>
+      
     </div>
   );
 };
