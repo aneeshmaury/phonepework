1d4cb56640fd35a967a6d5011db807b16a88b73d
aneeshmaury <aneeshmaury.in@gmail.com>
2025-01-11 09:48:35 +0530

    sdsaf

--- app/nav.js
+++ app/nav.js
@@ -224,9 +224,10 @@ function App() {
           onChange={(e) => setAccount(e.target.value)}
           placeholder="Enter account details"
         />
+        <AdBanner/>
       </div>
 
-      <AdBanner/>
+      
     </div>
   );
 }
