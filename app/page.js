0e22be4345f5b2f30554ec050f8e3a790d516e08
aneeshmaury <aneeshmaury.in@gmail.com>
2025-01-15 14:22:50 +0530

    uhiu

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderBar from "./nav";
-import BlockMotoPhones from "./block";
 
 const App = () => {
+  const [isBlocked, setIsBlocked] = useState(false);
+
   useEffect(() => {
     // Function to set the theme color
     const setThemeColor = () => {
@@ -22,16 +23,39 @@ const App = () => {
       metaTag.setAttribute("content", themeColor);
     };
 
+    // Function to detect Motorola devices
+    const blockMotoPhones = () => {
+      const userAgent = navigator.userAgent.toLowerCase();
+      if (userAgent.includes("moto")) {
+        setIsBlocked(true);
+      }
+    };
+
+    // Execute functions
     setThemeColor();
+    blockMotoPhones();
   }, []);
+
+  // If a Motorola device is detected, show the "Access Denied" message
+  if (isBlocked) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Access Denied</h1>
+        <p>Sorry, this website is not supported on Motorola devices.</p>
+      </div>
+    );
+  }
 
   return (
     <div>
       {/* Navigation Bar */}
       <HeaderBar />
 
-      {/* Block Moto Phones Logic */}
-      <BlockMotoPhones />
+      {/* Main Content */}
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Welcome to My Website</h1>
+        <p>This website works perfectly on your device!</p>
+      </div>
     </div>
   );
 };
