a5b668d6221b04a18935357068fb4abd34d04243
aneeshmaury <aneeshmaury.in@gmail.com>
2025-08-01 13:34:23 +0530

    rtret

--- app/nav.js
+++ app/nav.js
@@ -92,7 +92,7 @@ function App() {
           </div>
         </div>
         <div className="flex justify-end mb-2">
-          <div className="bg-[#f0eaf8] text-black p-3 rounded-tl-3xl rounded-bl-3xl rounded-br-3xl max-w-xs">
+          <div className="bg-[#f0eaf8] w-[250px] text-black p-3 rounded-tl-3xl rounded-bl-3xl rounded-br-3xl max-w-xs">
             <p>Made a payment to a fraudster for another reason</p>
             <div className="flex justify-end items-center">
               <span className="text-xs text-gray-500">{currentTime}</span>
@@ -120,7 +120,7 @@ function App() {
         </div>
 
         <div className="flex justify-start mb-2">
-          <div className="bg-gray-100 text-black p-3 rounded-tr-3xl rounded-br-3xl rounded-bl-3xl max-w-xs">
+          <div className="bg-gray-100 w-[280px] text-black p-3 rounded-tr-3xl rounded-br-3xl rounded-bl-3xl max-w-xs">
             <p>I understand how upsetting it is to lose your money, {name}.</p>
             <div className="flex justify-end items-center">
               <span className="text-xs text-gray-500">{currentTime}</span>
@@ -129,7 +129,7 @@ function App() {
         </div>
 
         <div className="flex justify-start mb-2">
-          <div className="bg-gray-100 text-black p-3 rounded-tr-3xl rounded-br-3xl rounded-bl-3xl max-w-xs">
+          <div className="bg-gray-100 w-[280px] text-black p-3 rounded-tr-3xl rounded-br-3xl rounded-bl-3xl max-w-xs">
             <p>As a security measure, we&apos;ll first verify your payment details and block the account to which you&apos;ve made this payment.</p>
             <div className="flex justify-end items-center">
               <span className="text-xs text-gray-500">{currentTime}</span>
@@ -147,7 +147,7 @@ function App() {
         </div>
 
         <div className="flex justify-start mb-2">
-          <div className="bg-gray-100 text-black p-3 rounded-tr-3xl rounded-br-3xl rounded-bl-3xl max-w-xs">
+          <div className="bg-gray-100 w-[280px] text-black p-3 rounded-tr-3xl rounded-br-3xl rounded-bl-3xl max-w-xs">
             <p>Could you please tell me how the fraudster contacted you?</p>
             <div className="flex justify-end items-center">
               <span className="text-xs text-gray-500">{currentTime}</span>
@@ -203,7 +203,8 @@ function App() {
 
         <div className="flex justify-start mb-2">
           <div className="bg-gray-100 w-[250px]  text-black p-3 rounded-tr-3xl rounded-br-3xl rounded-bl-3xl max-w-xs">
-            <p>We&apos;ve created the ticket {ticketNumber} for you. To view it, Tap? on the top right corner of your app&apos;s home screen and then tap My Tickets.</p>
+            
+            <p> We hope this information was helpful. <br/> <br/> We&apos;ve created the ticket {ticketNumber} for you. To view it, Tap? on the top right corner of your app&apos;s home screen and then tap My Tickets.</p>
             <div className="flex justify-end items-center">
               <span className="text-xs text-gray-500">{currentTime}</span>
             </div>
