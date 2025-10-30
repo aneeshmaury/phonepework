0213e6f0eff6c399b63cbd7a4aa6e8f638e2b4b4
Aneesh kumar maury <108884830+aneeshmaury@users.noreply.github.com>
2025-10-30 12:12:13 +0530

    Update nav.js

--- app/nav.js
+++ app/nav.js
@@ -18,9 +18,7 @@ const HeaderBar = () => {
           <p className="font-[18px]">VIEW TICKETS</p>
         </div>
       </div> */}
-      <div className="  flex items-center justify-between bg-white h-[60px] py-3 px-2 shadow-sm">
-      <div>  <img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*RjJVjIm_-ATx7gjNGXveWg.png" alt="PhonePe" width={43} className="rounded-md"  /> </div>
-
+      
       
     <div className="flex items-center gap-3 mr-6">
       <div>  <img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*RjJVjIm_-ATx7gjNGXveWg.png" alt="PhonePe" width={30} className="rounded-md"  /> </div>
