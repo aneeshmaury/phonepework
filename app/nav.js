f952a2a21f59767ef8ffba86ba60e39c204e3c37
aneeshmaury <aneeshmaury.in@gmail.com>
2025-12-09 08:06:56 +0530

    ewfwf

--- app/nav.js
+++ app/nav.js
@@ -30,7 +30,7 @@ const HeaderBar = () => {
 
 function App() {
   const [currentTime, setCurrentTime] = useState("");
-  const [name, setName] = useState("");
+  const [name, setName] = useState("Aneesh Kumar Maury");
   const [account, setAccount] = useState("");
   const [ticketNumber, setTicketNumber] = useState("");
 
