79f5e443b6082ce87c4a391d1ffe8ac3d430552f
aneeshmaury <aneeshmaury.in@gmail.com>
2025-12-18 09:03:37 +0530

    kjjhkj

--- app/nav.js
+++ app/nav.js
@@ -239,6 +239,22 @@ function App() {
             </div>
           </div>
         </div>
+        <div className="flex justify-end">
+          <div className="bg-[#f0eaf8] p-3  rounded-2xl max-w-xs text-black">
+            <p>No</p>
+            <div className="flex justify-end">
+              <span className="text-xs text-gray-500">{currentTime}</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-start">
+          <div className="bg-gray-100 p-3 rounded-2xl max-w-xs text-black">
+            <p> Thanks for letting us know.</p>
+            <div className="flex justify-end">
+              <span className="text-xs text-gray-500">{currentTime}</span>
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Input Fields */}
